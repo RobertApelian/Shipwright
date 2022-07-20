d86972fa08deac28ade6161cbb7fccd0cca42944
DefinedBehavior <defined@definedbehaviour.com>
2022-07-19 23:24:08 -0400

    Add the command map and some command types, figure out the command selection + execution setup

--- soh/soh/Enhancements/chaos.cpp
+++ soh/soh/Enhancements/chaos.cpp
@@ -6,20 +6,49 @@
 #include <z64.h>
 #include <variables.h>
 
+#include <functional>
+#include <map>
 #include <vector>
 #include <queue>
+#include <ctime>
+#include <string>
 
 #if defined(__linux__)
 #include "chaos_linux.h"
 #endif
 
-static bool g_is_enabled = false;
-static CommandStorage g_command_storage;
-
 template<typename T>
 T Read(const std::vector<uint8_t>& bytes, size_t start_index) {
 	return *((T*)(bytes.data() + start_index));
 }
+
+struct CommandCreator {
+	std::function<std::vector<uint8_t>(const std::vector<uint8_t>&)> read_payload_;
+	std::function<std::unique_ptr<ChaosCommand>(const std::vector<uint8_t>&)> create_;
+};
+
+static std::map<uint8_t, CommandCreator> kCommands {
+	{ 0x03, {
+				[](const std::vector<uint8_t>&) { return std::vector<uint8_t>({}); },
+				[](const std::vector<uint8_t>&) { 
+					return std::make_unique<OneShotCommand>([]() { gSaveContext.health = 0; }); 
+				}
+			}
+	},
+	{ 0x11, {
+				[](const std::vector<uint8_t>& bytes) { 
+					return std::vector<uint8_t>(bytes.begin() + 1, bytes.begin() + 1 + sizeof(uint32_t)); 
+				},
+				[](const std::vector<uint8_t>& payload) {
+					return std::make_unique<TimedBooleanCVarCommand>(
+						"gInvisPlayer", Read<uint32_t>(payload, 0));
+				}
+			}
+	}
+};
+
+static bool g_is_enabled = false;
+static CommandStorage g_command_storage;
 
 void Start() {
 	StartLinux();
@@ -33,19 +62,17 @@ void DisplayCommandMessage(const std::vector<uint8_t>& bytes, size_t start_index
 
 void EnqueueCommand(const std::vector<uint8_t>& bytes) {
 	size_t message_start_index = 1; // Account for the command idx itself
-	switch (bytes[0]) {
-		case 0x03: {
-			g_command_storage.AddCommand(std::make_unique<OneShotCommand>([](){
-			    gSaveContext.health = 0;
-			}));
-		    break;
-		}
-		default:
-			SohImGui::overlay->TextDrawNotification(10.0f, true, "Unrecognized command");
-			return;
+
+	auto it = kCommands.find(bytes[0]);
+	if (it == kCommands.end()) {
+		SohImGui::overlay->TextDrawNotification(10.0f, true, "Unrecognized command");
+		return;
 	}
 
-	DisplayCommandMessage(bytes, message_start_index);
+	auto payload = it->second.read_payload_(bytes);
+	g_command_storage.AddCommand(it->second.create_(payload));
+
+	DisplayCommandMessage(bytes, message_start_index + payload.size());
 }
 
 bool ReadBytes(size_t num, std::vector<uint8_t>* buf) {
--- soh/soh/Enhancements/chaos_commands.h
+++ soh/soh/Enhancements/chaos_commands.h
@@ -6,11 +6,27 @@
 #include <memory>
 #include <vector>
 
+#include <ctime>
+
+#include "ImGuiImpl.h"
+
+void Log(const std::string& msg) {
+	SohImGui::overlay->TextDrawNotification(10.0f, true, msg.c_str());
+}
 
 class ChaosCommand {
 	public:
 		// Called every frame, returns true if the command is still active after the tick, false otherwise
-		virtual bool Tick() = 0; 
+		bool Tick() {
+			if (CanTick()) {
+				return DoTick();
+			}
+			return true;
+		}
+
+		virtual bool DoTick() = 0;
+		virtual bool CanTick() { return true; }
+		virtual bool CanStart() { return true; }
 };
 
 class CommandStorage {
@@ -34,12 +50,65 @@ class OneShotCommand : public ChaosCommand {
 	public:
 		OneShotCommand(std::function<void()> f) : f_(f) {}
 
-		bool Tick() override {
+		bool DoTick() override {
 			f_();
 			return false;
 		}
 
 		std::function<void()> f_;
+};
+
+class TimedCommand : public ChaosCommand {
+	public:
+		TimedCommand(std::function<void()> tick_f, std::function<void()> cleanup_f, uint32_t seconds) 
+			: tick_f_(tick_f), cleanup_f_(cleanup_f), seconds_(seconds) {}
+
+		bool DoTick() override {
+			if (!start_time_) {
+				if (!CanStart()) return true;
+
+				start_time_ = time(nullptr);
+			}
+
+			if ((time(nullptr) - start_time_) >= seconds_) {
+				cleanup_f_();
+				return false;
+			}
+
+			tick_f_();
+			return true;
+		}
+
+		std::function<void()> tick_f_;
+		std::function<void()> cleanup_f_;
+
+		uint32_t seconds_;
+		time_t start_time_ = 0;
+};
+
+class TimedBooleanCVarCommand : public TimedCommand {
+	public:
+		TimedBooleanCVarCommand(const std::string& cvar, uint32_t seconds)
+			: cvar_(cvar), 
+			  TimedCommand(
+				[=]() { CVar_SetS32(cvar_.c_str(), 1); },
+				[=]() { CVar_SetS32(cvar_.c_str(), 0); },
+				seconds) {}
+
+		bool CanStart() override {
+			return !CVar_GetS32(cvar_.c_str(), 0);
+		}
+
+		std::string cvar_;
+};
+
+class OneShotBooleanCVarCommand : public OneShotCommand {
+	public:
+		OneShotBooleanCVarCommand(const std::string& cvar) 
+			: cvar_(cvar),
+			  OneShotCommand([=]() { CVar_SetS32(cvar_.c_str(), 1); }) {}
+
+		std::string cvar_;
 };
 
 #endif
